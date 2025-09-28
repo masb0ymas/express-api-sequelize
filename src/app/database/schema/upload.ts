d6e7710f747c00bf57a4ab1c6c71508cfce491cd
masb0ymas <n.fajri@outlook.com>
2025-09-28 13:07:23 +0700

    fix: adjust schema validation zod v4

--- src/app/database/schema/upload.ts
+++ src/app/database/schema/upload.ts
@@ -4,21 +4,19 @@ import BaseSchema from '../entity/base'
 // Schema
 export const uploadSchema = z.object({
   keyfile: z
-    .string({ required_error: 'keyfile is required' })
+    .string('keyfile is required')
     .min(3, { message: 'keyfile must be at least 3 characters long' }),
   filename: z
-    .string({ required_error: 'filename is required' })
+    .string('filename is required')
     .min(3, { message: 'filename must be at least 3 characters long' }),
   mimetype: z
-    .string({ required_error: 'mimetype is required' })
+    .string('mimetype is required')
     .min(3, { message: 'mimetype must be at least 3 characters long' }),
-  size: z
-    .number({ required_error: 'size is required' })
-    .min(1, { message: 'size must be at least 1' }),
+  size: z.number('size is required').min(1, { message: 'size must be at least 1' }),
   signed_url: z
-    .string({ required_error: 'signed_url is required' })
+    .string('signed_url is required')
     .min(3, { message: 'signed_url must be at least 3 characters long' }),
-  expiry_date_url: z.date({ required_error: 'expiry_date_url is required' }),
+  expiry_date_url: z.date('expiry_date_url is required'),
 })
 
 // Type
